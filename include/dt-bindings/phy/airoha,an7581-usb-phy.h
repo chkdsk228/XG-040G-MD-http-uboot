7a4ada8842c2eb625ea93a5942cbdce11efad595
Lz <chkdsk227@outlook.com>
2026-08-21 19:22:31 +0800

    Add missing Airoha dt-bindings headers from Linux kernel
    - en7523-clk.h: Clock IDs for EN7523/EN7581 SoCs
    - airoha,en7581-reset.h: Reset controller bindings
    - airoha,an7581-usb-phy.h: USB PHY bindings
    - airoha,scu-ssr.h: SCU SSR bindings
    
    Source: Linux kernel next tree

---
+++ include/dt-bindings/phy/airoha,an7581-usb-phy.h
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<title>kernel/git/next/linux-next.git - The linux-next integration testing tree</title>
+<meta name='generator' content='cgit 1.3-korg'/>
+<meta name='robots' content='noindex, nofollow'/>
+<link rel='stylesheet' type='text/css' href='/cgit-data/cgit.css'/>
+<script type='text/javascript' src='/cgit-data/cgit.js'></script>
+<link rel='shortcut icon' href='/favicon.ico'/>
+<link rel='alternate' title='Atom feed' href='http://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git/atom/?h=master' type='application/atom+xml'/>
+<link rel='vcs-git' href='git://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git' title='kernel/git/next/linux-next.git Git repository'/>
+<link rel='vcs-git' href='https://git.kernel.org/pub/scm/linux/kernel/git/next/linux-next.git' title='kernel/git/next/linux-next.git Git repository'/>
+<link rel='vcs-git' href='https://kernel.googlesource.com/pub/scm/linux/kernel/git/next/linux-next.git' title='kernel/git/next/linux-next.git Git repository'/>
+</head>
+<body>
+<div id='cgit'><table id='header'>
+<tr>
+<td class='logo' rowspan='2'><a href='/'><img src='/cgit-data/cgit.png' alt='cgit logo'/></a></td>
+<td class='main'><a href='/'>index</a> : <a href='/pub/scm/linux/kernel/git/next/linux-next.git/'>kernel/git/next/linux-next.git</a></td><td class='form'><form method='get'>
+<select name='h' onchange='this.form.submit();'>
---
+++ include/dt-bindings/reset/airoha,en7581-reset.h
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2024 AIROHA Inc
+ * Author: Lorenzo Bianconi <lorenzo@kernel.org>
+ */
+
+#ifndef __DT_BINDINGS_RESET_CONTROLLER_AIROHA_EN7581_H_
+#define __DT_BINDINGS_RESET_CONTROLLER_AIROHA_EN7581_H_
+
+/* RST_CTRL2 */
+#define EN7581_XPON_PHY_RST		 0
+#define EN7581_CPU_TIMER2_RST		 1
+#define EN7581_HSUART_RST		 2
+#define EN7581_UART4_RST		 3
+#define EN7581_UART5_RST		 4
+#define EN7581_I2C2_RST			 5
+#define EN7581_XSI_MAC_RST		 6
+#define EN7581_XSI_PHY_RST		 7
+#define EN7581_NPU_RST			 8
+#define EN7581_I2S_RST			 9
+#define EN7581_TRNG_RST			10
+#define EN7581_TRNG_MSTART_RST		11
+#define EN7581_DUAL_HSI0_RST		12
+#define EN7581_DUAL_HSI1_RST		13
+#define EN7581_HSI_RST			14
+#define EN7581_DUAL_HSI0_MAC_RST	15
+#define EN7581_DUAL_HSI1_MAC_RST	16
+#define EN7581_HSI_MAC_RST		17
+#define EN7581_WDMA_RST			18
+#define EN7581_WOE0_RST			19
+#define EN7581_WOE1_RST			20
+#define EN7581_HSDMA_RST		21
+#define EN7581_TDMA_RST			22
+#define EN7581_EMMC_RST			23
+#define EN7581_SOE_RST			24
+#define EN7581_PCIE2_RST		25
+#define EN7581_XFP_MAC_RST		26
+#define EN7581_USB_HOST_P1_RST		27
+#define EN7581_USB_HOST_P1_U3_PHY_RST	28
+/* RST_CTRL1 */
